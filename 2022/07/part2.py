724ae4d0a9eb6eddca829bcca82172f3d4bbadbb
Bruno Cardoso (Dança) <bruno.cardoso@buser.com.br>
2022-12-08 20:16:32 -0300

    .

---
+++ 2022/07/part2.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+# filename = "sample.txt"
+filename = "input.txt"
+
+data = open(filename).read().splitlines()
+
+curdir = ["<root>"]
+tree = defaultdict(int)
+
+for line in data:
+    items = line.split()
+
+    if items[0] == "$":
+        if items[1] == "cd":
+            folder = items[2]
+            if folder == "/":
+                curdir = ["<root>"]
+            elif folder == "..":
+                curdir.pop()
+            else:
+                curdir.append(folder)
+
+    elif items[0] == "dir":
+        pass
+
+    else:  # file
+        size = int(items[0])
+        li = curdir.copy()
+        while li:
+            tree["/".join(li)] += size
+            li.pop()
+
+required = tree["<root>"] - 40000000
+print(min(x for x in tree.values() if x >= required))
