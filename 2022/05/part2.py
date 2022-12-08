49bac4671eafc2f96398f613c08c06d76a735403
Bruno Cardoso (Dança) <bruno.cardoso@buser.com.br>
2022-12-08 01:22:52 -0300

    .

---
+++ 2022/05/part2.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+
+# filename = "sample.txt"
+filename = "input.txt"
+
+data = open(filename).read().split("\n\n")
+
+
+def parse_crates(data):
+    crates = defaultdict(list)
+    data = data.replace("    ", "[-] ")
+    data = data.replace(" ", "")
+    data = data.replace("[", "")
+    data = data.replace("]", "")
+    lines = reversed(data.splitlines()[:-1])
+    for line in lines:
+        for i, char in enumerate(line, start=1):
+            if char != "-":
+                crates[i].append(char)
+    return crates
+
+
+def parse_moves(data):
+    for line in data.splitlines():
+        _, qty, _, src, _, dst = line.split()
+        yield int(qty), int(src), int(dst)
+
+
+def move(crates, qty, src, dst):
+    items = []
+    for _ in range(qty):
+        items.append(crates[src].pop())
+    crates[dst].extend(reversed(items))
+
+
+crates = parse_crates(data[0])
+
+for qty, src, dst in parse_moves(data[1]):
+    move(crates, qty, src, dst)
+
+print("".join(x.pop() for x in crates.values()))
